5b31070fa4bfe8d512dc6db3db4c0f942d1d0a38
kasymovashakhida <kasymovashakhida@192.168.0.8>
2018-11-26 08:46:26 -0600

    test

---
+++ 1-My First Program/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Helo World!");
+		System.out.println(" My name is Shakhida");
+		System.out.print("How are you?");
+		System.out.println("I am good/Thanks");
+        System.out.println("Bye .Class is finished");
+		
+		
+
+	}
+
+}
